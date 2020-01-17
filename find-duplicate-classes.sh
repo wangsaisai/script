e3ea6a8cde681a7ffe24f543e058e50e3a1e76d0
wangsaisai <wangsaisai@users.noreply.github.com>
2020-01-17 15:49:47 +0800

    add usage

--- find-duplicate-classes.sh
+++ find-duplicate-classes.sh
@@ -1,2 +1,9 @@
+# usage example
+# bamboo@LM-SHB-24502256:~/workspaces/test/build/libs$find . -name '*.jar' -exec jar -tvf {} \; | grep WebConfig
+#  1443 Tue Nov 19 10:59:48 CST 2019 com/bamboo/config/WebConfig.class
+#  1443 Tue Nov 19 10:59:48 CST 2019 com/bamboo/config/WebConfig.class
+
+
+
 # Find duplicate classes in jar
 find . -name '*.jar' -exec jar -tvf {} \; | grep your-class-name
